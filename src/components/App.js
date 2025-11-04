e4e32fb2e29b78677af7ab505fd606333e43c30c
aftab-alam02526 <aftab0447@gmail.com>
2025-11-04 16:28:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,16 @@
-
-import React from "react";
-import { MemoryRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from "react";
 import Home from "./Home";
 import About from "./About";
 import Navigation from "./Navigation";
 
 function App() {
+  const [page, setPage] = useState("home");
+
   return (
-    <MemoryRouter>
-      <div>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </div>
-    </MemoryRouter>
+    <div className="app">
+      <Navigation setPage={setPage} />
+      {page === "home" ? <Home /> : <About />}
+    </div>
   );
 }
 
