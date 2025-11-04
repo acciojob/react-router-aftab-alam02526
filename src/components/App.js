84bccd50a2ab73d2b19a9b9a7227c65110b9a42c
aftab-alam02526 <aftab0447@gmail.com>
2025-11-04 16:34:09 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Home from "./Home";
 import About from "./About";
 import Navigation from "./Navigation";
 
 function App() {
-  const [page, setPage] = useState("home");
+  const [route, setRoute] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handleLocationChange = () => {
+      setRoute(window.location.pathname);
+    };
+    window.addEventListener("popstate", handleLocationChange);
+    return () => window.removeEventListener("popstate", handleLocationChange);
+  }, []);
+
+  const navigate = (path) => {
+    window.history.pushState({}, "", path);
+    setRoute(path);
+  };
 
   return (
     <div className="app">
-      <Navigation setPage={setPage} />
-      {page === "home" ? <Home /> : <About />}
+      <Navigation onNavigate={navigate} />
+      {route === "/" && <Home />}
+      {route === "/about" && <About />}
     </div>
   );
 }
 
 export default App;
+
 
 
